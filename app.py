2857df7f80aa6abb23a155071a109f92390061ff
Christopher Kittel <web@christopherkittel.eu>
2016-09-06 19:52:24 +0200

    new test setup

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ instructions on running.
 """
 from __future__ import print_function
 
+import sys
+import socket
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -13,6 +15,13 @@ from bokeh.embed import components, autoload_server
 from bokeh.client.session import pull_session, push_session
 
 app = Flask('contentmine-demo')
+
+## set up logging
+logger = logging.getLogger("applogger")
+sh_out = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+sh_out.setFormatter(formatter)
+logger.addHandler(sh_out)
 
 bokeh_url = "http://localhost:5006"
 applet_url = "http://localhost:5050"
@@ -26,6 +35,8 @@ def main():
 @app.route("/index")
 def applet():
     session = pull_session(url=bokeh_url, app_path="/interactive")
+    logger.info('session id: {0}'.format(session.id))
+    logger.info('socket: {0}'.format(socket.gethostbyname(socket.gethostname())))
     # session.pull()
     # session.loop_until_closed()
     script = autoload_server(None, session_id=session.id, app_path="/interactive")
