7796f997d4938fbc25d45f4f4a452b302d961b30
Christopher Kittel <web@christopherkittel.eu>
2016-09-07 22:03:57 +0200

    bokeh server now subprocess not in Procfile

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ from flask import Flask, render_template, request, redirect
 from bokeh.embed import components, autoload_server
 from bokeh.client.session import pull_session, push_session, ClientSession
 
+import subprocess
+import atexit
+
 app = Flask('contentmine-demo')
 
 ## set up logging
@@ -23,6 +26,16 @@ formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
 sh_out.setFormatter(formatter)
 logger.addHandler(sh_out)
 
+
+bokeh_process = subprocess.Popen(
+    ['bokeh', 'serve','--allow-websocket-origin=localhost:5000', '--allow-websocket-origin=0.0.0.0:5000', '--port=5100', 
+            'cooccurrences/cooccurrences.py', 'trending/trending.py', 'factexplorer/factexplorer.py', 'dictionaries/dictionaries.py'], stdout=subprocess.PIPE)
+
+# worker: bokeh serve cooccurrences/cooccurrences.py trending/trending.py factexplorer/factexplorer.py dictionaries/dictionaries.py --port=5100 --host=contentmine-demo.herokuapp.com --host=localhost:5006  --address=0.0.0.0 --use-xheaders --allow-websocket-origin=contentmine-demo.herokuapp.com --host=localhost:5100 --host=localhost:5000 --allow-websocket-origin=127.0.0.1:5000 --allow-websocket-origin=0.0.0.0:5000 --host=127.0.0.1
+
+@atexit.register
+def kill_server():
+    bokeh_process.kill()
 
 @app.route('/')
 def main():
