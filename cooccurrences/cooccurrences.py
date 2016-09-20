19d3cd3e6504a61ccf7c7452caaa646cb1834c49
Christopher Kittel <web@christopherkittel.eu>
2016-09-20 20:31:58 +0200

    tap tool now opens Wikidata URL

--- cooccurrences/cooccurrences.py
+++ cooccurrences/cooccurrences.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from bokeh.layouts import column, row, WidgetBox
 from bokeh.plotting import Figure, show
-from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.models import ColumnDataSource, HoverTool, OpenURL, TapTool
 from bokeh.models.widgets import Slider, Select, TextInput, DataTable, TableColumn, Paragraph, Div
 from bokeh.io import curdoc, output_notebook, output_file, save
 from bokeh.charts import HeatMap, bins, vplot
@@ -22,6 +22,10 @@ import gzip
 with gzip.open("coocc_factsets.pklz", "rb") as infile:
     factsets = pickle.load(infile)
 
+
+with gzip.open("../wikidata_dict.pklz", "rb") as infile:
+    wikidataIDs = pickle.load(infile)
+
 # Create Input controls
 pluginoptions = sorted([f[0] for f in list(factsets.keys())])
 top_n = Slider(title="Number of top-n items to display", value=10, start=5, end=25, step=5)
@@ -34,6 +38,7 @@ def update(attrname, old, new):
     new_selected, new_x_factors, new_y_factors = get_subset(dictionary_selector.value, dictionary_selector.value)
     bins = np.linspace(new_selected.counts.min(), new_selected.counts.max(), 10) # bin labels must be one more than len(colorpalette)
     new_selected["color"] = pd.cut(new_selected.counts, bins, labels = list(reversed(palettes.Blues9)), include_lowest=True)
+    new_selected["wikidataID"] = new_selected["x"].map(lambda x: wikidataIDs.get(x))
 
     p.xaxis.axis_label = dictionary_selector.value
     p.yaxis.axis_label = dictionary_selector.value
@@ -43,16 +48,19 @@ def update(attrname, old, new):
         x=new_selected["x"].astype(object),
         y=new_selected["y"].astype(object),
         color=new_selected["color"].astype(object),
+        wikidataID=new_selected["wikidataID"],
         raw=new_selected["raw"].astype(int)))
     source.data.update(src.data)
 
     p.x_range.update(factors=new_x_factors[:top_n.value])
     p.y_range.update(factors=new_y_factors[:top_n.value])
 
-source = ColumnDataSource(data=dict(x=[], y=[], color=[], raw=[]))
+source = ColumnDataSource(data=dict(x=[], y=[], color=[], raw=[], wikidataID=[]))
 selected, new_x_factors, new_y_factors = get_subset(dictionary_selector.value, dictionary_selector.value)
 
-TOOLS="tap, box_select, reset"
+TOOLS="tap, reset"
+
+
 
 p = Figure(plot_height=700, plot_width=700, title="",
            tools=TOOLS, toolbar_location="above",
@@ -63,6 +71,10 @@ p.xaxis.major_label_orientation = np.pi/4
 p.yaxis.major_label_orientation = np.pi/4
 p.xgrid.visible = False
 p.ygrid.visible = False
+
+url = "https://www.wikidata.org/wiki/@wikidataID"
+taptool = p.select(type=TapTool)
+taptool.callback = OpenURL(url=url)
 
 renderer = p.select(name="glyphs")[0]
 renderer.selection_glyph = renderer.glyph
