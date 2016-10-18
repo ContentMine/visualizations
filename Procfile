2eba681c1750267ce5023771d81e9910bd7d92ea
Christopher Kittel <web@christopherkittel.eu>
2016-10-18 11:30:35 +0200

    new test

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn app:app
-bokeh: bokeh serve cooccurrences/cooccurrences.py trending/trending.py factexplorer/factexplorer.py dictionaries/dictionaries.py --port=5006 --use-xheaders --host=127.0.0.1:5006 --address=127.0.0.1 --allow-websocket-origin=127.0.0.1:5006 --allow-websocket-origin=0.0.0.0:5000
+bokeh: bokeh serve cooccurrences/cooccurrences.py trending/trending.py factexplorer/factexplorer.py dictionaries/dictionaries.py --port=5006 --use-xheaders --host=127.0.0.1:5006 --address=127.0.0.1 --allow-websocket-origin=127.0.0.1:5006 --allow-websocket-origin=0.0.0.0:5000 --host=localhost:5006
--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def index():
 @app.route("/cooccurrences")
 def cooccurrences():
     # session = pull_session(url="http://localhost:5006/cooccurrences")
-    script = autoload_server(model=None, app_path="/cooccurrences", url="http://127.0.0.1:5006")
+    script = autoload_server(model=None, app_path="/cooccurrences")
     return render_template(
         "description.html",
         script = script,
@@ -82,7 +82,7 @@ def cooccurrences():
 @app.route("/trending")
 def trending():
     # session = pull_session(url="http://localhost:5006/trending")
-    script = autoload_server(model=None, app_path="/trending", url="http://127.0.0.1:5006")
+    script = autoload_server(model=None, app_path="/trending")
     return render_template(
         "description.html",
         title = "Exploring most frequent and uptrending facts",
@@ -93,7 +93,7 @@ def trending():
 @app.route("/dictionaries")
 def dictionaries():
     # session = pull_session(url="http://localhost:5006/dictionaries")
-    script = autoload_server(model=None, app_path="/dictionaries", url="http://127.0.0.1:5006")
+    script = autoload_server(model=None, app_path="/dictionaries")
     return render_template(
         "description.html",
         script = script,
@@ -104,7 +104,7 @@ def dictionaries():
 @app.route("/factexplorer")
 def factexplorer():
     # session = pull_session(url="http://localhost:5006/factexplorer")
-    script = autoload_server(model=None, app_path="/factexplorer", url="http://127.0.0.1:5006")
+    script = autoload_server(model=None, app_path="/factexplorer")
     return render_template(
         "description.html",
         script = script,
